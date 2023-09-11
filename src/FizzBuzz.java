ec8f6d360b74e9951a3d1126c962b3442531decb
yuxinduan20040830 <yuxinduan0830@gmail.com>
2023-09-11 17:00:29 -0400

    completed task 3.3

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -11,7 +11,7 @@ class FizzBuzz {
         }
     }
 
-    private static void doFizzBuzz(int i) {
+    static void doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
